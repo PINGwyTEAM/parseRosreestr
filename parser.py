f0a1ae7a5a28362a2b48e88233540fdcc7ba0215
Rostislaww <rostiki.com@mail.ru>
2021-04-11 01:12:39 +0300

    Updating some methods

--- parser.py
+++ parser.py
@@ -1,17 +1,19 @@
-import numpy as np
-import time, datetime
-
+import datetime
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
-from lxml import html
+from lxml.html import fromstring
 
 MAIN_PAGE = 'https://rosreestr.gov.ru/wps/portal/p/cc_ib_portal_services/online_request/'
 TO_FIND = ['Кадастровый номер:', 'Адрес (местоположение):', '(ОКС) Тип:', 'Площадь ОКС\'a:', 'Кадастровая стоимость:', 'Дата обновления информации:', 'Статус объекта:', 'Дата постановки на кадастровый учет:', 'Дата внесения стоимости:', 'Дата определения стоимости:']
 DICT = {'Кадастровый номер:':'0', 'Адрес (местоположение):':'1', '(ОКС) Тип:':'2', 'Площадь ОКС\'a:':'3', 'Кадастровая стоимость:':'4', 'Дата обновления информации:':'5', 'Статус объекта:':'6', 'Дата постановки на кадастровый учет:':'7', 'Дата внесения стоимости:':'8', 'Дата определения стоимости:':'9'}
+
+def log(comment):
+    print(str(datetime.datetime.now()), '\t', comment) #Ведение лога
 
 #Запуск Хрома
 def startChrome():
@@ -31,6 +33,7 @@ def wait_element (path):
 
 #Форматирование "некрасивых" строк
 def format(string):
+    string = string.replace('\n', '').replace('\xa0', ' ').split(' ')
     while '' in string:
         for char in range(len(string)):
             if string[char] == '':
@@ -40,40 +43,48 @@ def format(string):
 
 #Сбор данных со страницы
 def collect_data(source):
-    List = ['n/d' for a in range(12)]
+    List = ['n/d' for a in range(10)]
     try:
-        _count = len(source.xpath('//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/text()')) + 1
-        for i in range(_count):
+        _count = len(source.xpath('//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/text()'))
+        for i in range(1, _count + 1):
             try:
                 try:
-                    key = source.xpath(f'//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/tr[{i}]/td[1]/nobr/text()')[0].replace('\n', '').split(' ')
+                    key = source.xpath(f'//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/tr[{i}]/td[1]/nobr/text()')[0]
                 except:
-                    key = source.xpath(f'//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/tr[{i}]/td[1]/text()')[0].replace('\n', '').split(' ')
+                    key = source.xpath(f'//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/tr[{i}]/td[1]/text()')[0]
                 key = format(key)
-                value = format(source.xpath(f'//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/tr[{i}]/td[2]/b/text()')[0].replace('\n', '').split(' '))
+                value = format(source.xpath(f'//*[@id="layoutContainers"]/div[4]/div/div[2]/section/div[2]/div[2]/table/tbody/tr[5]/td/table/tbody/tr[{i}]/td[2]/b/text()')[0])
                 for _key in TO_FIND:
                     if _key in key:
                         List[int(DICT[_key])] = value
             except:
                 pass
-        _count = len(source.xpath('//*[@id="r_enc"]/table/tbody/text()')) + 1
-        for i in range(3, _count):
+
+        _count = len(source.xpath('//*[@id="r_enc"]/table/tbody/text()'))
+        for i in range(3, _count + 1, 2):
             try:
-                key_law = source.xpath(f'//*[@id="r_enc"]/table/tbody/tr[{i}]/td[1]/text()')[0].replace('\n', '').split(' ')
-                print(key_law)
-                if key_law != '':
-                    List[10] += format(key_law) + '; '
+                key_law = format(source.xpath(f'//*[@id="r_enc"]/table/tbody/tr[{i}]/td[1]/text()')[0])
+                if key_law == '':
+                    key_law += 'n/d'              
+                List += key_law + '\t'
             except:
                 pass
             try:
-                key_restriction = source.xpath(f'//*[@id="r_enc"]/table/tbody/tr[{i}]/td[2]/text()')[0].replace('\n', '').split(' ')
-                print(key_restriction)
-                if key_restriction != '':
-                    List[11] += format(key_restriction) + '; '
+                count_ = len(source.xpath(f'//*[@id="r_enc"]/table/tbody/tr[{i}]/td[2]/table/tbody/text()'))
+                print('cnt:', count_)
+                key_restriction = ''
+                if count_ == 0:
+                    key_restriction += 'n/d'
+                elif count_ == 1:
+                    key_restriction += format(source.xpath(f'//*[@id="r_enc"]/table/tbody/tr[{i}]/td[2]/table/tbody/tr/td/text()')[0])
+                else:
+                    for j in range(1, count_ + 1):
+                        key_restriction += format(source.xpath(f'//*[@id="r_enc"]/table/tbody/tr[{i}]/td[2]/table/tbody/tr[{j}]/td/text()')[0]) + '; '
+                List += key_restriction + '\t'
             except:
                 pass
     except:
-        print(str(datetime.datetime.now()), '\t', 'Incorrect page!') #Ведение лога
+        log('Incorrect page!')
     return List
 
 
@@ -84,31 +95,31 @@ def execute_request(number):
         inputField.clear()
         inputField.send_keys(number)
         driver.find_element_by_xpath('//*[@id="submit-button"]').send_keys(Keys.RETURN)
-        print(str(datetime.datetime.now()), '\t', 'Sending request!') #Ведение лога
-        time.sleep(1)
+        log('Sending request!')
+        sleep(1)
         try:
-            wait_element('//*[@id="js_oTr0"]/td[1]').click()
-            source = html.fromstring(driver.page_source)          
-            print(str(datetime.datetime.now()), '\t', 'Data found!') #Ведение лога
+            wait_element('//*[@id="js_oTr0"]/td[1]/a').click()
+            source = fromstring(driver.page_source)
+            log('Data found!')
 
             List = collect_data(source)
 
-            time.sleep(1)
+            sleep(1)
         except:
-            print(str(datetime.datetime.now()), '\t', 'Data not found!') #Ведение лога
+            log('Data not found!')
     except:
-        print(str(datetime.datetime.now()), '\t', 'Error sending request!') #Ведение лога
+        log('Error sending request!')
     return List
 
 driver = startChrome() #Запуск браузера
 driver.get(MAIN_PAGE) #Переход на веб-страницу ранее упомняутого сервиса
-print(str(datetime.datetime.now()), '\t', 'Page loaded!') #Ведение лога
+log('Page loaded!')
 
 #Циклическое извлечение кадастровых номеров объектов недвижимости из файла
 with open ('numbers.txt', 'r', encoding = 'utf-8') as File:
     COUNT = 1
     for Line in File:
-        print(str(datetime.datetime.now()), '\t', COUNT, '\t', Line.replace('\n', '')) #Ведение лога
+        log(Line.replace('\n', ''))
         List = execute_request(Line.replace('\n', ''))
 
         with open('Output.txt', 'a', encoding='utf-8') as OutputFile:
@@ -116,12 +127,12 @@ with open ('numbers.txt', 'r', encoding = 'utf-8') as File:
                 OutputFile.write(str(element) + '\t')
             OutputFile.write('\n')
 
-        print(str(datetime.datetime.now()), '\t', 'Done!') #Ведение лога
+        log('Done!')
         try:
             wait_element('//*[@id="js_es_1"]').click()
         except:
-            print(str(datetime.datetime.now()), '\t', 'No button!') #Ведение лога
+            log('No button!')
         COUNT += 1
 
-#Закрыте браузера после завершения цикла 
-driver.close()
+driver.close()#Закрыте браузера после завершения цикла 
+log('The program has been completed!')
